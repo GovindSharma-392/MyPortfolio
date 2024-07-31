64daf3fdefaa76b5dbbc4b47b1b76dc7fad90012
GovindSharma-392 <91676467+GovindSharma-392@users.noreply.github.com>
2024-07-31 13:25:27 +0530

    final

--- src/components/NavBars.js
+++ src/components/NavBars.js
@@ -7,12 +7,12 @@ const Navbar = () => {
     <nav className="navbar">
       <img src={logo} className="logo" alt="logo" />
       <span className="navtext">Govind Sharma</span>
-      <a href="/" className="nav-button scroll-link">About</a>
-      <a href="/Education" className="nav-button scroll-link">Vision</a>
-      <a href="/Experience" className="nav-button scroll-link">Experience</a>
-      <a href="/Projects" className="nav-button scroll-link">Projects</a>
-      <a href="/Skills" className="nav-button scroll-link">Skills</a>
-      <a href="/Hobbies" className="nav-button scroll-link">Hobbies</a>
+      <a href="#About" className="nav-button scroll-link">About</a>
+      <a href="#Education" className="nav-button scroll-link">Vision</a>
+      <a href="#Experience" className="nav-button scroll-link">Experience</a>
+      <a href="#Projects" className="nav-button scroll-link">Projects</a>
+      <a href="#Skills" className="nav-button scroll-link">Skills</a>
+      <a href="#Hobbies" className="nav-button scroll-link">Hobbies</a>
     </nav>
   );
 }
